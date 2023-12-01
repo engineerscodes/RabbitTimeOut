3892056b84f4b3147bcf401b51d1aac9ec6a0ab6
engineerscodes <practicalengineers05@gmail.com>
2023-12-01 18:15:13 +0530

    Test

--- src/main/java/org/test/app/rev.java
+++ src/main/java/org/test/app/rev.java
@@ -5,35 +5,52 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.concurrent.TimeoutException;
 
 
 public class rev {
     private final static String QUEUE_NAME = "Test_Rabbit_TimeOut";
 
-    public static void main(String[] argv) throws Exception {
+    public static void main(String[] argv){
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        Connection connection = null;
+        try {
+            connection = factory.newConnection();
 
-        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            System.out.println("[START] at  "+LocalDateTime.now());
-            String message = new String(delivery.getBody(), "UTF-8");
-            System.out.println(" [x] Received '" + message + "'");
-            try {
-                Thread.sleep(120000); // wait for 2 min
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-            System.out.println("[ENDS] at  "+ LocalDateTime.now());
-        };
-        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                System.out.println("[START] at  " + LocalDateTime.now());
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+                RestTemplate restTemplate = new RestTemplate();
+
+                // Define the URL you want to make a request to
+                String apiUrl = "http://localhost:9090/admin/downloadFile?id=3"; // this endpoint waits for 2 min and consumer timeout is 1
+
+                // Make a GET request and retrieve the response
+                ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
+
+                // Extract and print the response body
+                String responseBody = responseEntity.getBody();
+                System.out.println("Response Body: " + responseBody);
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                System.out.println("[ENDS] at  " + LocalDateTime.now());
+            };
+            channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> {
+            });
+        }catch (Exception e) {
+            System.out.println(e);
+        }
         //https://www.rabbitmq.com/consumers.html#acknowledgement-timeout
         // https://www.rabbitmq.com/tutorials/tutorial-two-java.html => under Message acknowledgment
     }
